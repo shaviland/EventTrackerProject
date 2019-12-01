7507142d2c169e63c2571a866fa45b4353bf16b8
Samantha Haviland <shaviland@live.com>
2019-12-01 11:15:59 -0700

    exercise entity done

--- DB/workoutdb.sql
+++ DB/workoutdb.sql
@@ -16,19 +16,19 @@ CREATE SCHEMA IF NOT EXISTS `workoutdb` DEFAULT CHARACTER SET utf8 ;
 USE `workoutdb` ;
 
 -- -----------------------------------------------------
--- Table `table1`
+-- Table `exercise`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `table1` ;
+DROP TABLE IF EXISTS `exercise` ;
 
-CREATE TABLE IF NOT EXISTS `table1` (
+CREATE TABLE IF NOT EXISTS `exercise` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `exercise` VARCHAR(200) NOT NULL,
+  `name` VARCHAR(200) NOT NULL,
   `type` VARCHAR(200) NOT NULL,
   `duration` INT NULL,
   `weight` INT NULL,
-  `place` VARCHAR(200) NULL,
   `num_reps` INT NULL,
   `num_sets` VARCHAR(45) NULL,
+  `place` VARCHAR(200) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -44,15 +44,15 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `table1`
+-- Data for table `exercise`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `workoutdb`;
-INSERT INTO `table1` (`id`, `exercise`, `type`, `duration`, `weight`, `place`, `num_reps`, `num_sets`) VALUES (1, 'walking', 'aerobic', 15, NULL, 'gym', NULL, NULL);
-INSERT INTO `table1` (`id`, `exercise`, `type`, `duration`, `weight`, `place`, `num_reps`, `num_sets`) VALUES (2, 'bicep curls', 'weight training', NULL, 45, 'gym', 12, '2');
-INSERT INTO `table1` (`id`, `exercise`, `type`, `duration`, `weight`, `place`, `num_reps`, `num_sets`) VALUES (3, 'lat pulls', 'weight training', NULL, 45, 'gym', 12, '2');
-INSERT INTO `table1` (`id`, `exercise`, `type`, `duration`, `weight`, `place`, `num_reps`, `num_sets`) VALUES (4, 'push ups', 'bodyweight training', NULL, NULL, 'home', 25, '3');
-INSERT INTO `table1` (`id`, `exercise`, `type`, `duration`, `weight`, `place`, `num_reps`, `num_sets`) VALUES (5, 'yoga', 'yoga', 60, NULL, 'yoga studio', NULL, NULL);
+INSERT INTO `exercise` (`id`, `name`, `type`, `duration`, `weight`, `num_reps`, `num_sets`, `place`) VALUES (1, 'walking', 'aerobic', 15, NULL, NULL, NULL, 'gym');
+INSERT INTO `exercise` (`id`, `name`, `type`, `duration`, `weight`, `num_reps`, `num_sets`, `place`) VALUES (2, 'bicep curls', 'weight training', NULL, 45, 12, '2', 'gym');
+INSERT INTO `exercise` (`id`, `name`, `type`, `duration`, `weight`, `num_reps`, `num_sets`, `place`) VALUES (3, 'lat pulls', 'weight training', NULL, 45, 12, '2', 'gym');
+INSERT INTO `exercise` (`id`, `name`, `type`, `duration`, `weight`, `num_reps`, `num_sets`, `place`) VALUES (4, 'push ups', 'bodyweight training', NULL, NULL, 25, '3', 'home');
+INSERT INTO `exercise` (`id`, `name`, `type`, `duration`, `weight`, `num_reps`, `num_sets`, `place`) VALUES (5, 'yoga', 'yoga', 60, NULL, NULL, NULL, 'yoga studio');
 
 COMMIT;
 
---
+++ JPAWorkoutTracker/src/main/java/com/skilldistillery/workout/entities/exercise.java
@@ -0,0 +1,177 @@
+package com.skilldistillery.workout.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class exercise {
+	
+	//FIELDS
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String type;
+	private int duration;
+	private int weight;
+	@Column(name = "num_reps")
+	private int numReps;
+	@Column(name = "num_sets")
+	private int numSets;
+	private String place;
+	
+	//CONSTRUCTORS
+	
+	public exercise(int id, String name, String type, int duration, int weight, int numReps, int numSets,
+			String place) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.duration = duration;
+		this.weight = weight;
+		this.numReps = numReps;
+		this.numSets = numSets;
+		this.place = place;
+	}
+
+	public exercise() {
+		super();
+	}
+
+	//GETTERS AND SETTERS
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getNumReps() {
+		return numReps;
+	}
+
+	public void setNumReps(int numReps) {
+		this.numReps = numReps;
+	}
+
+	public int getNumSets() {
+		return numSets;
+	}
+
+	public void setNumSets(int numSets) {
+		this.numSets = numSets;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	
+	//HASHCODE AND EQUALS
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + duration;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + numReps;
+		result = prime * result + numSets;
+		result = prime * result + ((place == null) ? 0 : place.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + weight;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		exercise other = (exercise) obj;
+		if (duration != other.duration)
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (numReps != other.numReps)
+			return false;
+		if (numSets != other.numSets)
+			return false;
+		if (place == null) {
+			if (other.place != null)
+				return false;
+		} else if (!place.equals(other.place))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		if (weight != other.weight)
+			return false;
+		return true;
+	}
+
+	//TOSTRING
+	
+	@Override
+	public String toString() {
+		return "exercise [id=" + id + ", name=" + name + ", type=" + type + ", duration=" + duration + ", weight="
+				+ weight + ", numReps=" + numReps + ", numSets=" + numSets + ", place=" + place + "]";
+	}
+	
+	
+	
+	
+}
